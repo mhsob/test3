6a14f9051b358772fc5c57d0189cd6a248ca27a3
mhsob <94296728+mhsob@users.noreply.github.com>
2021-11-14 21:37:20 +0100

    Aktualisiere pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,3 +1,13 @@
+function motor_vorwaerts () {
+    servos.P0.run(50)
+}
+function motor_drehe_rechts () {
+    servos.P0.run(50)
+}
 basic.forever(function () {
-	
+    if (grove.measureInCentimeters(DigitalPin.C16) < 50) {
+        motor_drehe_rechts()
+    } else {
+        motor_vorwaerts()
+    }
 })
