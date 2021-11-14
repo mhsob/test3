9701a8869991d8bc0a2aefc1e3855c37dc247fed
mhsob <94296728+mhsob@users.noreply.github.com>
2021-11-14 21:56:22 +0100

    Aktualisiere main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -2,7 +2,7 @@ function motor_vorwaerts () {
     servos.P0.run(50)
 }
 function motor_drehe_rechts () {
-    servos.P0.run(50)
+    servos.P3.run(50)
 }
 basic.forever(function () {
     if (grove.measureInCentimeters(DigitalPin.C16) < 50) {
